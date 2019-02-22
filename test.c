455d33535efc0027ab3316ed87a933933807a4b8
tsurutan <ishaninaritaina623@gmail.com>
2019-02-23 08:58:22 +0900

    create test

---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a = 1;
+    int *p_a = &a;
+    int **pp_a = &p_a;
+    printf("変数aのアドレスは%d\n",a);
+    printf("変数aのアドレスは%d\n",&a);
+    printf("変数aのポインタは%d\n",p_a);
+    printf("変数aのポインタは%d\n",*(p_a));
+    printf("変数aのポインタは%d\n",**(pp_a));
+    *p_a = 3;
+    printf("変数aのアドレスは%d\n",a);
+    return 0;
+}
