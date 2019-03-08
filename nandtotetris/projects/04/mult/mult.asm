0b51862045720c7bd0689866fdca294df0ed1f38
tsurutan <ishaninaritaina623@gmail.com>
2019-03-09 00:06:47 +0900

    create mult asm

--- nandtotetris/projects/04/mult/mult.asm
+++ nandtotetris/projects/04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @0
+  D=A
+  @2
+  M=D
+  @0
+  D=M
+  @END
+  D;JEQ
+  @i
+  M=D
+(LOOP)
+  @1
+  D=M
+  @2
+  M=D+M
+  @1
+  D=A
+  @i
+  M=M-D
+  D=M
+  @LOOP
+  D;JGT
+(END)
+  @END
+  0;JMP
